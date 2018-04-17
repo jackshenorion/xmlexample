9e41108becfc228afd1424891a708f8359df5af6
Jack Shen <shenpinyi@gmail.com>
2018-04-17 23:04:55 +1000

    add init code for sax and dom

---
+++ src/main/java/com/jackshenorion/xmlexample/dom/DomParser.java
@@ -0,0 +1,18 @@
+package com.jackshenorion.xmlexample.dom;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class DomParser {
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse("");
+    }
+}
---
+++ src/main/java/com/jackshenorion/xmlexample/sax/SaxParser.java
@@ -0,0 +1,21 @@
+package com.jackshenorion.xmlexample.sax;
+
+import com.sun.deploy.xml.XMLParser;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SaxParser {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        SAXParser saxParser = saxParserFactory.newSAXParser();
+        DefaultHandler handler = new XMLParser("");
+        saxParser.parse((InputStream) null, handler);
+    }
+}
