f6d93951b640186c094a9271bc8f2afb641cf7c8
Jack Shen <shenpinyi@gmail.com>
2018-04-18 23:11:07 +1000

    example of basic Dom and Sax parser

--- src/main/java/com/jackshenorion/xmlexample/dom/DomParser.java
+++ src/main/java/com/jackshenorion/xmlexample/dom/DomParser.java
@@ -1,6 +1,8 @@
 package com.jackshenorion.xmlexample.dom;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,6 +15,22 @@ public class DomParser {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse("");
+        Document document = documentBuilder.parse(DomParser.class.getClassLoader().getResource("full_example.xml").getPath());
+        NodeList childNodes = document.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i ++) {
+            printElement(childNodes.item(i), 1);
+        }
+    }
+
+    private static void printElement(Node node, int level) {
+        for (int i = 0; i < level; i ++) {
+            System.out.print("    ");
+        }
+        System.out.println(node);
+        NodeList childNodes = node.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i ++) {
+            printElement(childNodes.item(i), level + 1);
+        }
+
     }
 }
--- src/main/java/com/jackshenorion/xmlexample/sax/SaxParser.java
+++ src/main/java/com/jackshenorion/xmlexample/sax/SaxParser.java
@@ -1,21 +1,63 @@
 package com.jackshenorion.xmlexample.sax;
 
-import com.sun.deploy.xml.XMLParser;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class SaxParser {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
         SAXParser saxParser = saxParserFactory.newSAXParser();
-        DefaultHandler handler = new XMLParser("");
-        saxParser.parse((InputStream) null, handler);
+        int[] deep = {0};
+        DefaultHandler handler = new DefaultHandler() {
+            @Override
+            public void startDocument() throws SAXException {
+                super.startDocument();
+                printDeep(deep[0]);
+                System.out.println("startDocument");
+            }
+
+            @Override
+            public void endDocument() throws SAXException {
+                super.endDocument();
+                printDeep(deep[0]);
+                System.out.println("endDocument");
+            }
+
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                super.startElement(uri, localName, qName, attributes);
+                printDeep(deep[0]);
+                System.out.print("<" + qName + ">");
+                deep[0]++;
+            }
+
+            @Override
+            public void endElement(String uri, String localName, String qName) throws SAXException {
+                super.endElement(uri, localName, qName);
+                System.out.println("</" + qName + ">");
+                deep[0]--;
+            }
+
+            @Override
+            public void characters(char[] ch, int start, int length) throws SAXException {
+                super.characters(ch, start, length);
+                System.out.print(new String(ch, start, length));
+            }
+        };
+        saxParser.parse(new FileInputStream(SaxParser.class.getClassLoader().getResource("full_example.xml").getPath()), handler);
+    }
+
+    private static void printDeep(int deep) {
+        for (int i = 0; i < deep; i++) {
+            System.out.print("    ");
+        }
     }
 }
